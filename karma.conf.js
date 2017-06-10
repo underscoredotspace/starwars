369ec919ecad099261b3e007466d917b0d645a6b
ampersand <ampersand@underscore.space>
2017-06-10 19:40:39 +0100

    npm > yarn

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/angular/angular.min.js',
-      'bower_components/angular-mocks/angular-mocks.js',
+      'node_modules/angular/angular.min.js',
+      'node_modules/angular-mocks/angular-mocks.js',
       'app/src/*.js',
       'app/test/*.spec.js'
     ],
